8adbae5067652a8ff148397d588dbffcdbfd7cef
Darin755 <darinrawson@gmail.com>
2023-10-14 14:34:33 -0600

    added form spec

--- init.lua
+++ init.lua
@@ -1,12 +1,12 @@
  local getCommand = "GET"
- local receiveChannel = "" -- not implemented
- 
+ local receiveChannel = "update_stats" -- default channel
+print(os.time())
  -- print("we are loaded")
 
  -- called when message is recieved
  
 local on_digiline_receive = function (pos, _, channel, msg) 
-    if msg == getCommand then -- check if it is the right message
+    if channel == receiveChannel and msg == getCommand then -- check if it is the right message and channel
         -- print("uptime requested")
         local uptimeraw = minetest.get_server_uptime()
         local uptime = math.round(uptimeraw) -- round to the nearest second
@@ -20,11 +20,11 @@ local on_digiline_receive = function (pos, _, channel, msg)
         else
             uptimeMessage = "Up "..(math.round(uptime/86400)).." day(s)"
         end
-        digilines.receptor_send(pos, digilines.rules.default, channel, uptimeMessage)
+        digilines.receptor_send(pos, digilines.rules.default, receiveChannel, uptimeMessage) -- send data
     end
 end
 
-minetest.register_node("uptime_stats:stat_block", {
+minetest.register_node("uptime_stats:stat_block", { --register the node
 	description = "This is a block to get server uptime",
 	tiles = {
 		"node.png",
@@ -35,11 +35,22 @@ minetest.register_node("uptime_stats:stat_block", {
 		"node.png"
 	},
         groups = {dig_immediate=2},
-        digilines =
+        digilines = -- I don't rememeber why this is
 	{
 		receptor = {},
 		effector = {
-			action = on_digiline_receive
+			action = on_digiline_receive --on message recieved
 		},
 	},
+    after_place_node = function(pos, placer)
+		local meta = minetest.get_meta(pos)
+		meta:set_string("formspec",
+				"size[10,10]"..
+				"label[4,4;Channel]".. -- this is just a text label
+                "field[2,5;6,1;chnl;;]".. -- this is just the text entry box
+                "button_exit[4,6;2,1;exit;Save]") -- submit button, triggers on_receive_fields
+	end,
+    on_receive_fields = function(pos, formname, fields, player) -- to do: implement security
+        receiveChannel = fields.chnl --set the channel
+    end
 })
