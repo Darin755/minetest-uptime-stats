ce68e962b998c338b36f9eecf77233db29be1c35
DragonWrangler1 <146014546+DragonWrangler1@users.noreply.github.com>
2023-12-06 20:43:11 -0600

    Delete init.lua

--- init.lua
+++
@@ -1,9 +0,0 @@
-local path = minetest.get_modpath("stats")
-
---uptime block
-dofile(path.."/uptime.lua") -- uptime block
-dofile(path.."/time.lua") -- time block
-dofile(path.."/uptime_formatter.lua") -- uptime formatter block
-dofile(path.."/time_formatter.lua") -- time formatter block
-dofile(path.."/position.lua") -- position block
-dofile(path.."/getplayers.lua") -- lists players
